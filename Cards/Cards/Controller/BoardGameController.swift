9f2fc70dd60e9daf030e54a65d7e1abcfd0e9c99
victorKimpel() <mars.2874@gmail.com>
2023-03-18 23:52:03 +0300

    ...

--- Cards/Cards/Controller/BoardGameController.swift
+++ Cards/Cards/Controller/BoardGameController.swift
@@ -13,6 +13,14 @@ class BoardGameController: UIViewController {
     var cardsPairsCounts = 8
     // сущность "Игра"
     lazy var game: Game = getNewGame()
+    // кнопка для запуска/перезапуска игры
+    lazy var startButtonView = getStartButtonView()
+    
+    override func loadView() {
+        super.loadView()
+        // добавляем кнопку на сцену
+        view.addSubview(startButtonView)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +33,38 @@ class BoardGameController: UIViewController {
         game.cardsCount = self.cardsPairsCounts
         game.generateCards()
         return game
+    }
+    
+    private func getStartButtonView() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.center.x = view.center.x
+        
+        // получаем доступ к текущему окну
+        let window = UIApplication.shared.windows[0]
+        // определяем отступ сверху от границ окна до Safe Area
+        let topPadding = window.safeAreaInsets.top
+        // устанавливаем координату Y кнопки в соответствии с отступом
+        button.frame.origin.y = topPadding
+        
+        // устанавливаем текст
+        button.setTitle("Начать игру", for: .normal)
+        // устанавливаем цвет текста для обычного (не нажатого) состояния
+        button.setTitleColor(.black, for: .normal)
+        // устанавливаем цвет текста для нажатого состояния
+        button.setTitleColor(.gray, for: .highlighted)
+        // устанавливаем фоновый цвет
+        button.backgroundColor = .systemGray4
+        // скругляем углы
+        button.layer.cornerRadius = 10
+        
+        // подключаем обработчик нажатия на кнопку
+        button.addTarget(nil, action: #selector(startGame(_:)), for: .touchUpInside)
+        
+        return button
+    }
+    
+    @objc func startGame(_ sender: UIButton) {
+        print("button was pressed")
     }
     
 
